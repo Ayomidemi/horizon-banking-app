775190dc99d731eb2adf046d251efbdd56341dfd
Ayomidemi <adenijiayomide13@gmail.com>
2024-07-28 19:38:46 +0100

    Home page layout structuring

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,12 +1,31 @@
+import Image from "next/image";
+
+import Sidebar from "@/components/Sidebar";
+import { redirect } from "next/navigation";
+import MobileNav from "@/components/MobileNav";
+
 export default function RootLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
+  const loggedIn = { firstName: "Pease" } as User;
+
+  if (!loggedIn) redirect("/sign-in");
+
   return (
-    <main>
-      SIDEBAR
-      {children}
+    <main className="flex h-screen w-full font-inter">
+      <Sidebar user={loggedIn} />
+
+      <div className="flex size-full flex-col">
+        <div className="root-layout">
+          <Image src="/icons/logo.svg" width={30} height={30} alt="logo" />
+          <div>
+            <MobileNav user={loggedIn} />
+          </div>
+        </div>
+        {children}
+      </div>
     </main>
   );
 }
--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,7 +1,48 @@
+import HeaderBox from "@/components/HeaderBox";
+import RightSidebar from "@/components/RightSidebar";
+import TotalBalanceBox from "@/components/TotalBalanceBox";
 import React from "react";
 
 const HomePage = () => {
-  return <div>HomePage</div>;
+  const loggedIn = {
+    firstName: "Pease",
+    lastName: "Porridge",
+    email: "peaseporridge@gmail.com",
+  };
+
+  return (
+    <section className="home">
+      <div className="home-content">
+        <header className="home-header">
+          <HeaderBox
+            type="greeting"
+            title="Welcome"
+            user={loggedIn?.firstName || "Guest"}
+            subtext="Access and manage your account and transactions efficiently."
+          />
+
+          <TotalBalanceBox
+            accounts={[]}
+            totalBanks={1}
+            totalCurrentBalance={3455.99}
+          />
+        </header>
+
+        {/* <RecentTransactions
+          accounts={accountsData}
+          transactions={account?.transactions}
+          appwriteItemId={appwriteItemId}
+          page={currentPage}
+        /> */}
+      </div>
+
+      <RightSidebar
+        user={loggedIn}
+        transactions={[]}
+        banks={[{ currentBalance: 1350 }, { currentBalance: 850 }]}
+      />
+    </section>
+  );
 };
 
 export default HomePage;
